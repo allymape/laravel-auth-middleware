4cd4d49f4d209356068217dcbd04dd37e9c294f8
mape.ally <mape.ally@gmail.com>
2025-10-09 13:42:15 +0300

    fix Determine which version of permission check  to use

--- src/AuthMiddlewareServiceProvider.php
+++ src/AuthMiddlewareServiceProvider.php
@@ -33,10 +33,19 @@ class AuthMiddlewareServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/Resources/lang' => resource_path('lang/'),
         ], 'lang');
-
+        // Determine which version to use
+        $customMiddlewarePath = app_path('Http/Middleware/CheckPermission.php');
+        if (file_exists($customMiddlewarePath)) {
+            // Use the app version if published
+            require_once $customMiddlewarePath;
+            $checkPermissionClass = 'App\\Http\\Middleware\\CheckPermission';
+        } else {
+            // Fallback to package version
+            $checkPermissionClass = CheckPermission::class;
+        }
         // Register middleware aliases
         $this->app['router']->aliasMiddleware('api.key', ApiKeyMiddleware::class);
         $this->app['router']->aliasMiddleware('validate.token', ValidateToken::class);
-        $this->app['router']->aliasMiddleware('check.permission', CheckPermission::class);
+        $this->app['router']->aliasMiddleware('check.permission', $checkPermissionClass);
     }
 }
